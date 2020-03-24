53d1f08b4b72a2cdf91338c30d97e9836977d6c9
jeromewu <jeromewus@gmail.com>
2020-03-24 02:21:56 +0000

    Update example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { createWorker } from '@ffmpeg/ffmpeg';
 import './App.css';
 
 function App() {
+  const [videoSrc, setVideoSrc] = useState('');
+  const [message, setMessage] = useState('Click Start to transcode');
+  const worker = createWorker();
+  const doTranscode = async () => {
+    setMessage('Loading ffmpeg-core.js');
+    await worker.load();
+    setMessage('Start transcoding');
+    await worker.write('test.avi', '/flame.avi');
+    await worker.transcode('test.avi', 'test.mp4');
+    setMessage('Complete transcoding');
+    const { data } = await worker.read('test.mp4');
+    setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p/>
+      <video src={videoSrc} controls></video><br/>
+      <button onClick={doTranscode}>Start</button>
+      <p>{message}</p>
     </div>
   );
 }
