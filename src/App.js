7d72ad87768c5adff6f4833faed3e5a02e68303d
Jerome Wu <jeromewus@gmail.com>
2020-04-28 20:05:49 +0800

    Upgrade to latest version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { createWorker } from '@ffmpeg/ffmpeg';
+import React, { useState } from 'react';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
 import './App.css';
 
 function App() {
   const [videoSrc, setVideoSrc] = useState('');
   const [message, setMessage] = useState('Click Start to transcode');
-  const worker = createWorker();
+  const ffmpeg = createFFmpeg({
+    log: true,
+    corePath: '/ffmpeg-core.js',
+  });
   const doTranscode = async () => {
     setMessage('Loading ffmpeg-core.js');
-    await worker.load();
+    await ffmpeg.load();
     setMessage('Start transcoding');
-    await worker.write('test.avi', '/flame.avi');
-    await worker.transcode('test.avi', 'test.mp4');
+    await ffmpeg.write('test.avi', '/flame.avi');
+    await ffmpeg.transcode('test.avi', 'test.mp4');
     setMessage('Complete transcoding');
-    const { data } = await worker.read('test.mp4');
+    const data = ffmpeg.read('test.mp4');
     setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
   };
   return (
